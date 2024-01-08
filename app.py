5a545a3f00ec8e07bbd5914b4824b9dd348c8ef0
Guilherme Duarte <gui.martins.duarte@gmail.com>
2024-01-08 13:00:39 -0300

    Separar as classes, criar sistema de avaliações, corrigir erro do "def ativo".

---
+++ app.py
@@ -0,0 +1,12 @@
+from restaurante import Restaurante       #importa do restaurante.py a classe Restaurante
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca.receber_avaliacao('Gui', 10)
+restaurante_praca.receber_avaliacao('Laís', 8)
+restaurante_praca.receber_avaliacao('Emy', 5)
+
+def main():
+    Restaurante.listar_restaurantes()
+
+if __name__ == '__main__':
+    main()
--- restaurante.py
+++ restaurante.py
@@ -1,21 +1,41 @@
+from avaliacao import Avaliacao
+
 class Restaurante:
     restaurantes = []
 
-    def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+    def __init__(self, nome='', categoria='', ativo=False):
+        self.nome = nome.title()
+        self.categoria = categoria.upper()
+        self._ativo = False
+        self.avaliacao = []
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
     
-    def listar_restaurantes():
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
-            #não usa o SELF. porque já ta puxando a informação direto da lista que armazena os objetos.
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
 
-restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+    @property
+    def ativo(self):
+        return '⌧' if self._ativo else '☐'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
-Restaurante.listar_restaurantes
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao = Avaliacao(cliente, nota)
+        self._avaliacao.append(avaliacao)
+
+    @property           #Torna possível ler essas informações.
+    def media_avaliacoes(self):
+        if not self.avaliacao:
+            return 0
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        #pega a NOTA de todas as avalicoes que existam em self._avaliacao. 
+        quantidade_de_nostas = len(self._avaliacao)
+        media = round(soma_das_notas / quantidade_de_nostas, 1)
+        return media
