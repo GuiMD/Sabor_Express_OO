c18ff65fb95dd9a41d4cedb0a331d8dcfd339460
Guilherme Duarte <gui.martins.duarte@gmail.com>
2024-01-09 21:38:26 -0300

    adicionar avaliação no listar_restaurantes e arrumar o código

--- restaurante.py
+++ restaurante.py
@@ -7,7 +7,7 @@ class Restaurante:
         self.nome = nome.title()
         self.categoria = categoria.upper()
         self._ativo = False
-        self.avaliacao = []
+        self._avaliacao = []
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
@@ -15,9 +15,9 @@ class Restaurante:
     
     @classmethod
     def listar_restaurantes(cls):
-        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} | {'Status'}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
@@ -32,7 +32,7 @@ class Restaurante:
 
     @property           #Torna possível ler essas informações.
     def media_avaliacoes(self):
-        if not self.avaliacao:
+        if not self._avaliacao:
             return 0
         soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
         #pega a NOTA de todas as avalicoes que existam em self._avaliacao. 
